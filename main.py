f153f2c1bee6b88fbafd92e324ab6ddb318bc6f8
Alejandro <medinalex13@gmail.com>
2017-12-08 21:56:31 -0500

    Working on API Configuration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests #requestsb library for HTTP requests
+import json
 
 payload = {
 "key" : "API_KEY_GOES_HERE", #API Key goes here
@@ -12,3 +13,10 @@ payload = {
 
 url = "https://pixabay.com/api/"
 response = requests.get(url,params = payload)
+
+outfile = open "/home/justusenumbers/Projects/The_Background_Project/AfterRequest.txt", "w")
+
+
+response.json()
+jsonToPython = json.loads(response)
+print(jsonToPython)
