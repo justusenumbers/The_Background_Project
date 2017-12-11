d33c9aedbfec05ced2f328387b35d98f4cf71030
Alejandro <medinalex13@gmail.com>
2017-12-10 22:29:18 -0500

    Finished Pixabay API functionality. Added Script for downloading and saving images.

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import json
 
 payload = {
 "key" : "API_KEY_GOES_HERE", #API Key goes here
+"q" : "Wallpaper",
 "image_type":"photo",
 "category" : "backgrounds",
 "min_width" : "960",
 "min_height" : "720",
 "editors_choice" : "True",
+"per_page" : "10",
 "safesearch" : "True"
 }
 
 url = "https://pixabay.com/api/"
 response = requests.get(url,params = payload)
 
-outfile = open "/home/justusenumbers/Projects/The_Background_Project/AfterRequest.txt", "w")
+#ADD TRY STATEMENT FOR WHEN API NOT PRESENT
 
 
-response.json()
-jsonToPython = json.loads(response)
-print(jsonToPython)
+for pictureURL in json.loads(response.text.encode("utf-8"))["hits"]:
+     with open ("/home/justusenumbers/Projects/The_Background_Project/res/"+str(pictureURL["id"])+".jpg", "wb") as outfile:
+         outfile.write(requests.get(pictureURL["userImageURL"]).content)
+         print (pictureURL["id"], "Successfully Douwnloaded!" )
